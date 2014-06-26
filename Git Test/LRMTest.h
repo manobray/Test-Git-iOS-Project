187094b40b90302856b72f1d7d4b37bbbc7b48c5
Lucas Maciel <manobray@iMac-de-Lucas.local>
2014-06-26 17:02:34 -0300

    added a test class

---
+++ Git Test/LRMTest.h
@@ -0,0 +1,13 @@
+//
+//  LRMTest.h
+//  Git Test
+//
+//  Created by Lucas Maciel on 6/26/14.
+//  Copyright (c) 2014 Lucas Maciel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LRMTest : NSObject
+
+@end
---
+++ Git Test/LRMTest.m
@@ -0,0 +1,13 @@
+//
+//  LRMTest.m
+//  Git Test
+//
+//  Created by Lucas Maciel on 6/26/14.
+//  Copyright (c) 2014 Lucas Maciel. All rights reserved.
+//
+
+#import "LRMTest.h"
+
+@implementation LRMTest
+
+@end
